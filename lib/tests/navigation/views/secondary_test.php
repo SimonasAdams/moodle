3b03d1a8d65d5411121bb38d362c89e3225da2f1
Simon Adams <simon.adams@catalyst-eu.net>
2024-04-09 17:22:46 +0100

    WIP remove/rework redundant code

--- lib/tests/navigation/views/secondary_test.php
+++ lib/tests/navigation/views/secondary_test.php
@@ -969,10 +969,9 @@ class secondary_test extends \advanced_testcase {
                 'id',
                 false
             ],
-            //MDL-71378 TODO: Deprecate this
             "Question bank should return nothing" => [
                 '/question/edit.php',
-                'courseid',
+                'cmid',
                 false
             ],
             "Reports should return nothing" => [
