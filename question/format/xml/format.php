2b216987cbae06db2f48aa24f36cf89fb2558067
Simon Adams <simon.adams@catalyst-eu.net>
2024-04-12 11:40:05 +0100

    fixup! fixup! fixup! WIP remove/rework redundant code

--- question/format/xml/format.php
+++ question/format/xml/format.php
@@ -1553,7 +1553,7 @@ class qformat_xml extends qformat_default {
 
             if (!empty($tagobjects)) {
                 $context = context::instance_by_id($contextid);
-                $sortedtagobjects = question_sort_tags($tagobjects, $context, [$this->course]);
+                $sortedtagobjects = question_sort_tags($tagobjects, $context);
 
                 if (!empty($sortedtagobjects->coursetags)) {
                     // Set them on the form to be rendered as existing tags.
