4bf3140f06df345ddbb13b84ec180d51390334f8
Simon Adams <simon.adams@catalyst-eu.net>
2024-08-02 10:00:25 +0100

    MDL-71378 mod_qbank: Add install script to transfer question categories

--- question/edit.php
+++ question/edit.php
@@ -65,6 +65,9 @@ $PAGE->set_heading($COURSE->fullname);
 $PAGE->activityheader->disable();
 
 echo $OUTPUT->header();
+if (!\mod_qbank\task\transfer_question_categories::has_completed_successfully()) {
+    echo $OUTPUT->notification(get_string('transfernotfinished', 'mod_qbank'), \core\output\notification::NOTIFY_WARNING);
+}
 
 // Print horizontal nav if needed.
 $renderer = $PAGE->get_renderer('core_question', 'bank');
